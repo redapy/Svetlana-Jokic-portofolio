c0db3821fbf44c278abef200e58332d0ccc73aed
redapy <baha.redaaaa@gmail.com>
2021-10-07 14:04:36 +0100

    refactor the list of lesson plans using the map methode

--- src/components/lessonPlans/LessonPlans.js
+++ src/components/lessonPlans/LessonPlans.js
@@ -8,6 +8,7 @@ import Logoimg from '../../images/lesson-plan-logo.png'
 import Logomicro from '../../images/micro-lesson-logo.png'
 
 
+
 const LessonPlans = () => {
     return (
         <>
@@ -19,16 +20,9 @@ const LessonPlans = () => {
                         <h3>Lesson PLans</h3>
                     </Flex>
                     <List>
-                        <li><a href="https://docs.google.com/document/d/1d7GURxczRjvXuX3IZh4x6JgPAAHnPhmk/edit?rtpof=true" target="_blank" rel="noreferrer">Edited Reading/Writing Lesson Plan with Technology </a></li>
-                        <li><a href="https://docs.google.com/document/d/1szm0tKSekaxn-cWQOel2LElxMawamyp5/edit?rtpof=true" target="_blank" rel="noreferrer">Technology-Enriched Reading/Writing Lesson Plan </a></li>
-                        <li><a href="https://docs.google.com/document/d/1KsXoFfE4gwyKqfz2qA4LPyhXC9ZhA0Mx/edit" target="_blank" rel="noreferrer">Edited Listening/Speaking Lesson Plan with Technology</a></li>
-                        <li><a href="https://docs.google.com/document/d/1yyiYxVmmWNWZKsxQ03piUA05Qd9UOIgm/edit?rtpof=true" target="_blank" rel="noreferrer">Technology-Enriched Listening/Speaking Lesson Plan</a></li>
-                        <li><a href="https://docs.google.com/document/d/1AXX3S9ouvfCr_czvutFkiMMiZKgrYoqs/edit " target="_blank" rel="noreferrer">Grammar Lesson Plan</a></li>
-                        <li><a href="https://docs.google.com/document/d/1PE9iuXqKecDjYN2QMbofpGsm2WZaujb8/edit" target="_blank" rel="noreferrer">Pronunciation Lesson Plan</a></li>
-                        <li><a href="https://docs.google.com/document/d/1CJxQjYL7Ba4jruhbtkmF2bkjD4v4QlNi/edit" target="_blank" rel="noreferrer">Capstone 1 Lesson Plan  Micro-lesson</a></li>
-                        <li><a href="https://docs.google.com/document/d/1dz6EULqIqwM2AAqaprkVk2xoq6L3oyxJ/edit?usp=sharing&ouid=117204858719934608213&rtpof=true&sd=true" target="_blank" rel="noreferrer">Original Reading/Writing/Grammar Lesson Plan</a></li>
-                        <li><a href="https://docs.google.com/document/d/1gbSDdI7S9U2VXaJM5nGmbScRosmBIW9I/edit" target="_blank" rel="noreferrer">Original Listening/Speaking/Pronunciation Lesson Plan</a></li>
-                        <li><a href="https://docs.google.com/document/d/13bbFhlJZAL_x306sqzutvZcSsQl57XwD/edit" target="_blank" rel="noreferrer">Technology-Enriched Lesson Plan</a></li>
+                        {lessons.map(lesson => (
+                            <li key={lesson.id}><a href={lesson.link} target="_blank" rel="noreferrer">{lesson.title}</a></li>
+                        ))}
                     </List>
                 </Content>
                 <Content>
@@ -37,16 +31,9 @@ const LessonPlans = () => {
                         <h3>Micro lessons</h3>
                     </Flex>
                     <List>
-                        <li><a href="https://youtu.be/ZyJEW6ZbuGY" target="_blank" rel="noreferrer">Edited Reading/Writing Lesson Plan with Technology </a></li>
-                        <li><a href="https://www.youtube.com/watch?v=c7RReha9_bE&t=9s" target="_blank" rel="noreferrer">Technology-Enriched Reading/Writing Lesson Plan </a></li>
-                        <li><a href="https://youtu.be/T4ZID8_x4kU" target="_blank" rel="noreferrer">Edited Listening/Speaking Lesson Plan with Technology</a></li>
-                        <li><a href="https://youtu.be/mu86mYiL9Bs" target="_blank" rel="noreferrer">Technology-Enriched Listening/Speaking Lesson Plan</a></li>
-                        <li><a href="https://youtu.be/fhLI5NGE4U8" target="_blank" rel="noreferrer">Grammar Lesson Plan</a></li>
-                        <li><a href="https://youtu.be/xoiwyYFvbzo" target="_blank" rel="noreferrer">Pronunciation Lesson Plan</a></li>
-                        <li><a href="https://youtu.be/cfm8F6BqZ-E" target="_blank" rel="noreferrer">Capstone 1 Lesson Plan  Micro-lesson</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=0U1H_K0A38w" target="_blank" rel="noreferrer">Original Reading/Writing/Grammar Lesson Plan</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=06NdA9NoFDQ&t=190s" target="_blank" rel="noreferrer">Original Listening/Speaking/Pronunciation Lesson Plan</a></li>
-                        <li><a href="https://www.youtube.com/watch?v=ptqihNQsV2I&t=2s" target="_blank" rel="noreferrer">Technology-Enriched Lesson Plan</a></li>
+                        {lessons.map(lesson => (
+                            <li key={lesson.id}><a href={lesson.link} target="_blank" rel="noreferrer">{lesson.title}</a></li>
+                        ))}
                     </List>
                 </Content>
             </Wrapper>
@@ -55,3 +42,66 @@ const LessonPlans = () => {
 }
  
 export default LessonPlans;
+
+const lessons = [
+    {
+        title: 'Edited Reading/Writing Lesson Plan with Technology ',
+        link: "https://docs.google.com/document/d/1d7GURxczRjvXuX3IZh4x6JgPAAHnPhmk/edit?rtpof=true",
+        video: 'https://youtu.be/ZyJEW6ZbuGY',
+        id:1
+    },
+     {
+        title: 'Technology-Enriched Reading/Writing Lesson Plan',
+        link: 'https://docs.google.com/document/d/1szm0tKSekaxn-cWQOel2LElxMawamyp5/edit?rtpof=true',
+        video: 'https://www.youtube.com/watch?v=c7RReha9_bE&t=9s',
+        id:2
+    },
+     {
+        title: 'Edited Listening/Speaking Lesson Plan with Technology ',
+        link: 'https://docs.google.com/document/d/1KsXoFfE4gwyKqfz2qA4LPyhXC9ZhA0Mx/edit',
+        video: 'https://youtu.be/T4ZID8_x4kU',
+        id:4
+    },
+    {
+        title: 'Technology-Enriched Listening/Speaking Lesson Plan ',
+        link: 'https://docs.google.com/document/d/1yyiYxVmmWNWZKsxQ03piUA05Qd9UOIgm/edit?rtpof=true',
+        video: 'https://youtu.be/mu86mYiL9Bs',
+        id:5
+    },
+    {
+        title: 'Grammar Lesson Plan ',
+        link: 'https://docs.google.com/document/d/1AXX3S9ouvfCr_czvutFkiMMiZKgrYoqs/edit',
+        video: 'https://youtu.be/fhLI5NGE4U8',
+        id:6
+    },
+    {
+        title: 'Pronunciation Lesson Plan',
+        link: 'https://docs.google.com/document/d/1PE9iuXqKecDjYN2QMbofpGsm2WZaujb8/edit',
+        video: 'https://youtu.be/xoiwyYFvbzo',
+        id: 7
+    },
+    {
+        title: 'Capstone 1 Lesson Plan  Micro-lesson',
+        link: 'https://docs.google.com/document/d/1CJxQjYL7Ba4jruhbtkmF2bkjD4v4QlNi/edit',
+        video: 'https://youtu.be/cfm8F6BqZ-E',
+        id: 8
+    },
+    {
+        title: 'Original Reading/Writing/Grammar Lesson Plan  ',
+        link: 'https://docs.google.com/document/d/1dz6EULqIqwM2AAqaprkVk2xoq6L3oyxJ/edit?usp=sharing&ouid=117204858719934608213&rtpof=true&sd=true',
+        video: 'https://www.youtube.com/watch?v=0U1H_K0A38w',
+        id:9
+    },
+    {
+        title: 'Original Listening/Speaking/Pronunciation Lesson Plan',
+        link: 'https://docs.google.com/document/d/1gbSDdI7S9U2VXaJM5nGmbScRosmBIW9I/edit',
+        video: 'https://www.youtube.com/watch?v=06NdA9NoFDQ&t=190s',
+        id:10
+    },
+    {
+        title: 'Technology-Enriched Lesson Plan',
+        link: 'https://docs.google.com/document/d/13bbFhlJZAL_x306sqzutvZcSsQl57XwD/edit',
+        video: 'https://www.youtube.com/watch?v=ptqihNQsV2I&t=2s',
+        id:11
+    },
+]
